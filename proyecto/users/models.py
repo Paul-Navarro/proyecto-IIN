0d63fade29fa27b41688795a20b0c8c86475e314
Diego Torres <ditocapy@fpuna.edu.py>
2024-08-30 20:14:51 -0400

    agregación de roles y permisos, con sus respectivas tablas

--- proyecto/users/models.py
+++ proyecto/users/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-
+"""
 class CustomUser(AbstractUser):
-    """
+    '''
     Modelo personalizado de usuario que extiende el modelo de usuario predeterminado de Django (AbstractUser).
     Este modelo permite agregar campos adicionales y personalizar la relación con grupos y permisos.
-    """
+    '''
 
     # Campo de correo electrónico único para cada usuario.
     email = models.EmailField(unique=True)
@@ -37,8 +37,56 @@ class CustomUser(AbstractUser):
     )
 
     def __str__(self):
-        """
+        '''
         Devuelve una representación en cadena del usuario, mostrando su nombre de usuario.
         Esto es útil en la interfaz administrativa y en otros contextos donde se necesite una representación legible.
-        """
+        '''
         return self.username
+"""
+
+#clase para los roles
+class Role(models.Model):
+    """Modelo de Rol para gestionar roles dinámicos."""
+    name = models.CharField(max_length=50, unique=True)  # Nombre del rol
+    permissions = models.ManyToManyField(Permission, blank=True)  # Permisos asociados al rol
+
+    def __str__(self):
+        return self.name
+
+#clase de usuario modificado
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    roles = models.ManyToManyField(Role, blank=True, related_name='users')  # Relación con roles
+
+    def __str__(self):
+        return self.username
+
+    def has_role(self, role_name):
+        """
+        Comprueba si el usuario tiene un rol específico.
+        """
+        return self.roles.filter(name=role_name).exists()
+
+    def get_permissions(self):
+        """
+        Obtiene todos los permisos asociados a los roles del usuario.
+        """
+        permissions = super().get_user_permissions()
+        for role in self.roles.all():
+            permissions |= set(role.permissions.values_list('codename', flat=True))
+        return permissions
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
