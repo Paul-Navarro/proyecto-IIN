b28b6fa56a353034a7bb0f6a9bf70bb8fc8877d5
Crista Orue <corumx26@gmail.com>
2024-10-04 12:09:41 -0400

    arreglo de redireccion

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'leticia',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '212612',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -230,7 +230,11 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
 SESSION_COOKIE_AGE = 1209600  # 2 semanas, en segundos para que no se desloguee el usuario al pasar a stripe
+
 SESSION_SAVE_EVERY_REQUEST = True #asegura que la sesión se renueve con cada solicitud del usuario
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # No expira cuando se cierra el navegador
 
+# Configuración de seguridad de cookies
+SESSION_COOKIE_SECURE = False  # Cambiar a True si usas HTTPS en producción
+CSRF_COOKIE_SECURE = False  # Cambiar a True si usas HTTPS en producción
